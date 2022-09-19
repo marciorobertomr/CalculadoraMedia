761cfecf5e55f033c188fa9ff28767129ec646b5
Márcio Roberto <marcio_mr@hotmail.com>
2022-09-18 23:00:16 -0300

    field validation

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+var validates;
+
 function calc() {
-    var fristBimester = parseFloat(document.getElementById("fristBimester").value);
-    var secondBimester = parseFloat(document.getElementById("secondBimester").value);
-    var thirdBimester = parseFloat(document.getElementById("thirdBimester").value);
-    var forthBimester = parseFloat(document.getElementById("forthBimester").value);
+    validation();
+    if (validates == true) {
+        var fristBimester = parseFloat(document.getElementById("fristBimester").value);
+        var secondBimester = parseFloat(document.getElementById("secondBimester").value);
+        var thirdBimester = parseFloat(document.getElementById("thirdBimester").value);
+        var forthBimester = parseFloat(document.getElementById("forthBimester").value);
 
-    var average = (fristBimester + secondBimester + thirdBimester + forthBimester)/4;
-    var approval;
+        var average = (fristBimester + secondBimester + thirdBimester + forthBimester)/4;
+        var approval;
 
-    if (average >= 7) {
-        approval = "Aprovado";
-    } else {
-        approval = "Reprovado";
+        if (average >= 7) {
+            approval = "Aprovado";
+        } else {
+            approval = "Reprovado";
+        }
+
+        var result = document.getElementById("result");
+        resultAverage = "<span>A sua média foi: " + average + ". <br>Você foi " + approval + "!";
+        result.innerHTML = resultAverage;
     }
-
-    var result = document.getElementById("result");
-    resultAverage = "A sua média foi: " + average + ". <br>Você foi " + approval + "!";
-    result.innerHTML = resultAverage;
 }
 
 function reset() {
@@ -23,4 +28,30 @@ function reset() {
     document.getElementById("secondBimester").value = "";
     document.getElementById("thirdBimester").value = "";
     document.getElementById("forthBimester").value = "";
+    
+}
+
+function validation() {
+    var fristBimester = parseFloat(document.getElementById("fristBimester").value);
+    var secondBimester = parseFloat(document.getElementById("secondBimester").value);
+    var thirdBimester = parseFloat(document.getElementById("thirdBimester").value);
+    var forthBimester = parseFloat(document.getElementById("forthBimester").value);
+
+    var grades = [fristBimester, secondBimester, thirdBimester, forthBimester];
+
+    for (var i = 0; i < grades.length; i++) {
+        if (Number.isNaN(grades[i]) == false) { //Valida se o valor retornado (number) é NaN (Not-a-Number)
+            if (grades[i] <= 10 && grades[i] >= 0) {
+                validates = true;
+            } else {
+                alert("Notas incorretas. Por gentileza, revise-as. Obs: As notas devem ser de 0 a 10.");
+                validates = false;
+                i = grades.length;
+            }
+        } else {
+            alert("Campos vazios/inválidos. Por gentileza, revise-os e informe a nota corretamente.");
+            validates = false;
+            i = grades.length;
+        }
+    }
 }
