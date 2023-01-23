e3e5d5dd22444141119f558980422a3e7e0906d4
marciorobertomr <marcio_mr1@hotmail.com>
2023-01-22 22:43:02 -0300

    Ajuste do JS

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var validates;
+let validates;
 
 function calc() {
-    validation();
-    if (validates == true) {
-        var fristBimester = parseFloat(document.getElementById("fristBimester").value);
-        var secondBimester = parseFloat(document.getElementById("secondBimester").value);
-        var thirdBimester = parseFloat(document.getElementById("thirdBimester").value);
-        var forthBimester = parseFloat(document.getElementById("forthBimester").value);
+    let fristBimester = parseFloat(document.getElementById("fristBimester").value);
+    let secondBimester = parseFloat(document.getElementById("secondBimester").value);
+    let thirdBimester = parseFloat(document.getElementById("thirdBimester").value);
+    let forthBimester = parseFloat(document.getElementById("forthBimester").value);
 
-        var average = (fristBimester + secondBimester + thirdBimester + forthBimester)/4;
-        var approval;
+    validation(fristBimester, secondBimester, thirdBimester, forthBimester);
+
+    if (validates == true) {
+        let average = (fristBimester + secondBimester + thirdBimester + forthBimester) / 4;
+        let approval;
 
         if (average >= 7) {
             approval = "Aprovado";
@@ -17,7 +18,7 @@ function calc() {
             approval = "Reprovado";
         }
 
-        var result = document.getElementById("result");
+        let result = document.getElementById("result");
         resultAverage = "<div id='return' class='result'><span>A sua média foi: " + average + ". <br>Você foi " + approval + "!</span></div>";
         result.innerHTML = resultAverage;
     }
@@ -32,20 +33,16 @@ function reset() {
     document.getElementById("return").remove();    
 }
 
-function validation() {
-    var fristBimester = parseFloat(document.getElementById("fristBimester").value);
-    var secondBimester = parseFloat(document.getElementById("secondBimester").value);
-    var thirdBimester = parseFloat(document.getElementById("thirdBimester").value);
-    var forthBimester = parseFloat(document.getElementById("forthBimester").value);
+function validation(fristBimester, secondBimester, thirdBimester, forthBimester) {
 
-    var grades = [fristBimester, secondBimester, thirdBimester, forthBimester];
+    let grades = [fristBimester, secondBimester, thirdBimester, forthBimester];
 
-    for (var i = 0; i < grades.length; i++) {
+    for (let i = 0; i < grades.length; i++) {
         if (Number.isNaN(grades[i]) == false) { //Valida se o valor retornado (number) é NaN (Not-a-Number)
             if (grades[i] <= 10 && grades[i] >= 0) {
                 validates = true;
             } else {
-                alert("Notas incorretas. Por gentileza, revise-as. Obs: As notas devem ser de 0 a 10.");
+                alert("Notas incorretas. Por gentileza, revise-as. Obs: As notas devem estar entre 0 e 10.");
                 validates = false;
                 i = grades.length;
             }
